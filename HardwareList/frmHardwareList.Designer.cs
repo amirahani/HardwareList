7521342681e89d628dfdd48aeb6449556ffcde97
Amir Ahani <amirahani@gmail.com>
2019-06-29 20:33:34 -0700

    Added Hardware List Project

---
+++ HardwareList/frmHardwareList.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace HardwareList
+{
+    partial class frmHardwareList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHardwareList = new System.Windows.Forms.Button();
+            this.lstHardwareList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnHardwareList
+            // 
+            this.btnHardwareList.Location = new System.Drawing.Point(34, 32);
+            this.btnHardwareList.Name = "btnHardwareList";
+            this.btnHardwareList.Size = new System.Drawing.Size(129, 23);
+            this.btnHardwareList.TabIndex = 0;
+            this.btnHardwareList.Text = "Get Hardware List";
+            this.btnHardwareList.UseVisualStyleBackColor = true;
+            this.btnHardwareList.Click += new System.EventHandler(this.btnHardwareList_Click);
+            // 
+            // lstHardwareList
+            // 
+            this.lstHardwareList.FormattingEnabled = true;
+            this.lstHardwareList.Location = new System.Drawing.Point(34, 62);
+            this.lstHardwareList.Name = "lstHardwareList";
+            this.lstHardwareList.Size = new System.Drawing.Size(1055, 459);
+            this.lstHardwareList.TabIndex = 1;
+            // 
+            // frmHardwareList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 571);
+            this.Controls.Add(this.lstHardwareList);
+            this.Controls.Add(this.btnHardwareList);
+            this.Name = "frmHardwareList";
+            this.Text = "Hardware List";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHardwareList;
+        private System.Windows.Forms.ListBox lstHardwareList;
+    }
+}
+
---
+++ HardwareList/frmHardwareList.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace HardwareList
+{
+    public partial class frmHardwareList : Form
+    {
+        public frmHardwareList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHardwareList_Click(object sender, EventArgs e)
+        {
+
+            lstHardwareList.Items.Add("Printer - Hardware Devices");
+
+            ManagementScope objScope = new ManagementScope(ManagementPath.DefaultPath); //For the local Access
+            objScope.Connect();
+
+            SelectQuery selectQuery = new SelectQuery();
+            selectQuery.QueryString = "Select * from win32_Printer";
+            ManagementObjectSearcher MOS = new ManagementObjectSearcher(objScope, selectQuery);
+            ManagementObjectCollection MOC = MOS.Get();
+            foreach (ManagementObject mo in MOC)
+            {
+                lstHardwareList.Items.Add(mo["Name"].ToString());
+            }
+
+            lstHardwareList.Items.Add("USB - Hardware Devices");
+
+            var usbDevices = GetUSBDevices();
+
+            foreach (var usbDevice in usbDevices)
+            {
+                // lstHardwareList.Items.Add(String.Format("Device ID: {0}, PNP Device ID: {1}, Description: {2}",
+                //    usbDevice.DeviceID, usbDevice.PnpDeviceID, usbDevice.Description));
+                lstHardwareList.Items.Add(usbDevice.Description);
+            }
+
+
+
+
+        }
+
+
+
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+
+    }
+
+    class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+}
